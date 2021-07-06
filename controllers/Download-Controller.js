b513b4041fd23c17b1ce0f472c681e9790aa73d6
kaveenakash <kaveenakashya@gmail.com>
2021-07-06 12:18:23 +0530

    update download routes new

--- controllers/Download-Controller.js
+++ controllers/Download-Controller.js
@@ -44,7 +44,7 @@ const DeleteTemplate = async(req,res,next) =>{
     const {id} = req.body
    
     try {
-      const response = await User.findOneAndRemove({_id:id})
+      const response = await Template.findOneAndRemove({_id:id})
       return res.status(200).json(response)
     } catch (err) {
       const error = new HttpError("Unexpected Error Occured", 503);
